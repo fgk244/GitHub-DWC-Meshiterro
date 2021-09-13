02a5440b913987b0535ea1415ccf7ce29078c1aa
fgk244 <fgk.244@gmail.com>
2021-09-13 05:40:05 +0000

    comit13

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2282c9af9de228ff343aafd2611643dc060994083ec040d6148282bf468e1a85aa91420168c667059cffaf6f82bd4a6af3df78d28619018d33fce1465b82b897'
